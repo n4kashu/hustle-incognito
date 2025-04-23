fa9a05a85edb0bf1a0daabdebe5cef1930d068c5
Shannon Code <genecyber@gmail.com>
2025-04-23 05:03:59 -0400

    fix: Rename .eslintrc.js to .eslintrc.cjs for ESM compatibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,23 @@
-// .eslintrc.cjs
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier', // Make sure this is last to override other configs
   ],
-  rules: {
-    'prettier/prettier': 'warn', // Show Prettier issues as warnings
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Warn on unused vars, ignore if prefixed with _
-    '@typescript-eslint/explicit-module-boundary-types': 'off', // Allow implicit return types for functions
-    '@typescript-eslint/no-explicit-any': 'warn', // Warn on explicit 'any'
-    // Add any project-specific rules here
-  },
+  plugins: ['@typescript-eslint'],
   env: {
     node: true,
-    es2021: true,
+    browser: true,
+    es6: true,
   },
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_' 
+    }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.cjs', '*.config.js', '*.config.cjs'],
+  ignorePatterns: ['dist', 'node_modules', '*.js'],
 };
